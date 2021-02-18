0fd4fbd3b1ef6d5aec2d2013689cb23fa0db31fa
michaelk <kaltenecker.m@gmx.de>
2021-02-18 22:59:56 +0100

    updated lot of stuff

--- nano-rs/src/accounts_balances.rs
+++ nano-rs/src/accounts_balances.rs
@@ -1,35 +1,36 @@
-// use serde_json::{Map};
 
-// #[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// pub struct Request {
-//     pub action: String,
-//     pub accounts: Vec<String>,
-// }
 
-// #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// pub struct Result {
-//     balances: Map<String, Balance>,
-// }
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {
+    pub action: String,
+    pub accounts: Vec<String>,
+}
 
-// #[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
-// #[serde(rename_all = "camelCase")]
-// pub struct Balance {
-//     pub balance: String,
-//     pub pending: String,
-// }
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Result {
+    pub balances: Vec<AccountBalance>,
+}
 
-// impl Request {
-//     pub fn as_vec(&self) -> Vec<u8> {
-//         let encoded: Vec<u8> = serde_json::to_vec(&self).unwrap();
-//         encoded
-//     }
-// }
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AccountBalance {
+    pub balance: String,
+    pub pending: String,
+}
 
-// impl Result {
-//     pub fn as_vec(&self) -> Vec<u8> {
-//         let encoded: Vec<u8> = serde_json::to_vec(&self).unwrap();
-//         encoded
-//     }
-// }
+
+impl Request {
+    pub fn as_vec(&self) -> Vec<u8> {
+        let encoded: Vec<u8> = serde_json::to_vec(&self).unwrap();
+        encoded
+    }
+}
+
+impl Result {
+    pub fn as_vec(&self) -> Vec<u8> {
+        let encoded: Vec<u8> = serde_json::to_vec(&self).unwrap();
+        encoded
+    }
+}
